ab736f475b94d2af1a393e1d75bbcf45393964f8
zegolem <zegolem1383@outlook.fr>
2021-12-01 10:09:03 +0100

    day01/part1
    count the number of times a depth measurement increases

---
+++ day1/part1.cpp
@@ -0,0 +1,24 @@
+// g++ part1.cpp -std=c++20 -Wall -Wextra -pedantic -o part1
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	(void)argc, (void)argv;
+	int last_measurement = -1;
+	int new_measurement = 0;
+	int increased_count = 0;
+	while (std::cin >> new_measurement) {
+		if (last_measurement == -1) {
+			std::cout << new_measurement << " (N/A - no previous measurement)" << std::endl;
+			last_measurement = new_measurement;
+			continue;
+		}
+		int diff = new_measurement - last_measurement;
+		std::cout << new_measurement << " " << ((diff > 0) ? "(increased)" : "(decreased)")
+		          << std::endl;
+		if (diff > 0) ++increased_count;
+		last_measurement = new_measurement;
+	}
+	std::cout << "Increased " << increased_count << " times" << std::endl;
+}
